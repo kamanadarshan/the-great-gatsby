938e95c4cbfa83856268763f2401dec803f14ebc
kamanadarshan <darshan.kamana@gmail.com>
2021-12-02 12:20:42 -0500

    Finished Gatsby tutorial part2

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,23 +1,19 @@
+
 // Step 1: Import React
 import * as React from 'react'
 
 import Layout from '../components/layout'
 
-
 // Step 2: Define your component
-const IndexPage = () => {
+const AboutPage = () => {
   return (
-    <main>
-      
-      <Layout pageTitle="Home Page">
-      <p>I'm making this by following the Gatsby Tutorial.</p>
+    <Layout pageTitle="About Me">
+      <p>Hi there! I'm the proud creator of this site, which I built with Gatsby.</p>
     </Layout>
-      
-    </main>
   )
 }
 
 
 
 // Step 3: Export your component
-export default IndexPage
+export default AboutPage
---
+++ src/pages/say-hello.js
@@ -0,0 +1,17 @@
+//import * as React from 'react'
+
+// Rendering the <Greeting> component
+// const SayHello = () => {
+//   return (
+//     <div>
+//       <Greeting name="Megan" />
+//       <Greeting name="Obinna" />
+//       <Greeting name="Generosa" />
+//     </div>
+//   )
+// }
+
+
+
+// Step 3: Export your component
+//export default Say-HelloPage
