591b192b374aa5a9c9f4d1875caba02bf68b4d43
kamanadarshan <darshan.kamana@gmail.com>
2021-12-10 14:10:12 -0500

    Gatsby tutorial 6 complete

---
+++ src/pages/blog/index.js
@@ -0,0 +1,48 @@
+import * as React from 'react'
+import Layout from '../../components/layout'
+import { Link, graphql } from 'gatsby'
+//import {MDXRenderer} from 'gatsby-plugin-mdx'
+
+const BlogPage = ({data}) => {
+  return (
+    <Layout pageTitle="My Blog Posts">
+      <p>My cool posts will go in here</p>
+      
+      {
+        data.allMdx.nodes.map(node => (
+          <article key={node.id}>
+            <h2>{node.frontmatter.title}</h2>
+            <Link to={`/blog/${node.slug}`}>
+                {node.frontmatter.title}
+              </Link>
+            <p>Posted:{node.frontmatter.date}</p>
+            {/* <MDXRenderer>
+              {node.body}
+            </MDXRenderer> */}
+
+
+          </article>
+        ))
+      }
+       
+    </Layout>
+  )
+}
+
+export const query = graphql`
+  query {
+    allMdx(sort: {order: ASC, fields: frontmatter___date}) {
+      nodes {
+        frontmatter {
+          title
+          date(formatString: "MMMM D, YYYY")
+        }
+        id
+        slug
+      }
+    }
+  }
+`
+ 
+
+export default BlogPage
